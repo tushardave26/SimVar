a80f26998331ac95abfbb566a5da47567a8734f7
Tushar Dave <tushar.dave@duke.edu>
2019-04-15 16:57:24 -0400

    Modify the setup tool script

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version='0.0.1',
     author='Tushar Dave',
     author_email='tushardave26@gmail.com',
-    py_modules=['simvar.py'],
+    py_modules=['simvar'],
     install_requires=[
         "click==7.0",
         "pysam==0.15.2",
     ],
     entry_points='''
         [console_scripts]
-        simvar=simvar.py:cli
+        simvar=simvar:main
     ''',
 )
